2bf6901dddcb9c0535e7056e4ec6b0f2864d8053
Jimmy Zettler <jgzettler@crimson.ua.edu>
2024-05-16 00:36:28 -0400

    This commit is just utilizing the fmt function in two different ways, demonstrating that Fprintln is the same as println with a predefined first argument

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+//	"net/http"
+	"os"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-}
+//func handlerFunc(w http.ResponseWriter, r *http.Request) {
+//	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+//}
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+//	http.HandleFunc("/", handlerFunc)
+	fmt.Fprintln(os.Stdout, "Hello, World!")
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+//	http.ListenAndServe(":3000", nil)
 }
-// Testing
