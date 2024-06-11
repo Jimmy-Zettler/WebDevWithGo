d29893f4c54b19b3a4cfbf68a5e0136d9bf61cd5
Jimmy Zettler <jgzettler@crimson.ua.edu>
2024-06-11 16:28:48 -0500

    Update main.go

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"fmt"
-//	"net/http"
-	"os"
+	"net/http"
 )
 
-//func handlerFunc(w http.ResponseWriter, r *http.Request) {
-//	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-//}
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
 func main() {
-//	http.HandleFunc("/", handlerFunc)
-	fmt.Fprintln(os.Stdout, "Hello, World!")
+	http.HandleFunc("/", handlerFunc)
 	fmt.Println("Starting the server on :3000...")
-//	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", nil)
 }
